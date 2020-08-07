72803a4aad7ed22c6b7b74e86af188eb7484ac40
Tithi-Paul <tithi.cse3.bu@gmail.com>
2020-08-08 02:32:57 +0600

    Day_2: Syntax, Variables, and Numbers

---
+++ Day_2/swap.py
@@ -0,0 +1,7 @@
+a = [1, 2, 3]
+b = [3, 2, 1]
+
+temp = a
+a = b
+b = temp
+print(a, b)
---
+++ Day_2/variable.py
@@ -0,0 +1,11 @@
+# Try_ something  challenging & learn a lot at once.
+Flag = 0
+Flag = Flag + 4
+
+if Flag > 0:
+    print("It is a positive number")
+
+Multi = "Multi " * Flag
+print(Multi)
+
+

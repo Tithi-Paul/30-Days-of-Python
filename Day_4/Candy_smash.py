84147ecb7c02d52318260fc856a6f6b8279a26ad
Tithi-Paul <tithi.cse3.bu@gmail.com>
2020-08-09 02:09:37 +0600

    Day_4: Booleans and Conditionals

---
+++ Day_4/Candy_smash.py
@@ -0,0 +1,9 @@
+def to_smash(total_candies):
+    if total_candies == 1:
+        print("Splitting", total_candies, "candy")
+    else:
+         print("Splitting", total_candies, "candies")
+    return total_candies % 3
+
+to_smash(91)
+to_smash(1)

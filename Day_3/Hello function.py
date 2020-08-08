fe4c1ac68ce11ae9b06a9e2b877a1f2c55c2c392
Tithi-Paul <tithi.cse3.bu@gmail.com>
2020-08-08 16:07:28 +0600

    Day_3: Functions

---
+++ Day_3/Hello function.py
@@ -0,0 +1,7 @@
+def greet(who="Tithi"):
+    print("Hello,", who)
+    
+greet()
+greet(who="Paul")
+# (In this case, we don't need to specify the name of the argument, because it's unambiguous.)
+greet("Python!")
---
+++ Day_3/Number of smash candies.py
@@ -0,0 +1,4 @@
+def to_smash(total_candies, n_friends=3):
+    return total_candies % n_friends
+
+print(to_smash(92))
